234a2eb92f95e22d0c3959091d08f2dc4bef6584
Marcelo Pouso <mpouso@emtech.com.ar>
2020-09-05 23:14:06 -0300

    removed comments

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,7 +14,6 @@ Java_com_mapx_kosten_basicnativeccalculator_MainActivity_stringFromJNI(
 extern "C" JNIEXPORT jfloat JNICALL
 Java_com_mapx_kosten_basicnativeccalculator_MainActivity_add( JNIEnv *env, jobject, jfloat x, jfloat y) {
 
-    // return an integer
     return x + y;
 }
 
@@ -22,23 +21,18 @@ Java_com_mapx_kosten_basicnativeccalculator_MainActivity_add( JNIEnv *env, jobje
 extern "C" JNIEXPORT jfloat JNICALL
 Java_com_mapx_kosten_basicnativeccalculator_MainActivity_sub( JNIEnv *env, jobject, jfloat x, jfloat y) {
 
-    // return an integer
     return x - y;
 }
 
 // Multiplication function
 extern "C" JNIEXPORT jfloat JNICALL
 Java_com_mapx_kosten_basicnativeccalculator_MainActivity_multiply( JNIEnv *env, jobject, jfloat x, jfloat y) {
-
-    // return an integer
     return x * y;
 }
 
 //Division function
 extern "C" JNIEXPORT jfloat JNICALL
 Java_com_mapx_kosten_basicnativeccalculator_MainActivity_divide( JNIEnv *env, jobject, jfloat x, jfloat y) {
-
-    // return an integer
     if (y>0) {
         return x / y;
     }
